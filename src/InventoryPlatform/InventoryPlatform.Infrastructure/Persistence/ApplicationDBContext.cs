dfbb483e4b6dab32953f661fa3192932844600e8
crisantoiii <crisantotadiosaiii@gmail.com>
2026-07-16 20:08:47 +0800

    add initial application configuration

---
+++ src/InventoryPlatform/InventoryPlatform.Infrastructure/Persistence/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryPlatform.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
