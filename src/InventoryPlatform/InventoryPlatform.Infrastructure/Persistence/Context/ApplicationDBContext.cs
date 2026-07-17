97f191d7022f80acbb9adfa353cf6f235088b124
crisantoiii <crisantotadiosaiii@gmail.com>
2026-07-17 17:51:34 +0800

    add local development tools configuration

--- src/InventoryPlatform/InventoryPlatform.Infrastructure/Persistence/Context/ApplicationDBContext.cs
+++ src/InventoryPlatform/InventoryPlatform.Infrastructure/Persistence/Context/ApplicationDBContext.cs
@@ -2,12 +2,19 @@
 
 namespace InventoryPlatform.Infrastructure.Persistence.Context;
 
-public sealed class ApplicationDbContext
-    : DbContext
+public sealed class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(
         DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(ApplicationDbContext).Assembly);
     }
 }
