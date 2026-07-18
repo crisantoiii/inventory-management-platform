36e280c02d051504291ff743a95156339a1f2bbd
crisantoiii <crisantotadiosaiii@gmail.com>
2026-07-18 20:22:41 +0800

    add application dependency injection configuration

---
+++ src/InventoryPlatform/InventoryPlatform.Application/DependencyInjection/ServiceCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using InventoryPlatform.Application.Features.Products.CreateProduct;
+using InventoryPlatform.Application.Features.Products.GetProduct;
+using InventoryPlatform.Application.Features.Products.GetProducts;
+using InventoryPlatform.Application.Features.Products.UpdateProduct;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InventoryPlatform.Application.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddScoped<CreateProductHandler>();
+        services.AddScoped<GetProductHandler>();
+        services.AddScoped<GetProductsHandler>();
+        services.AddScoped<UpdateProductHandler>();
+
+        services.AddValidatorsFromAssembly(
+            typeof(ServiceCollectionExtensions).Assembly);
+
+        return services;
+    }
+}
