fb9c64648692c63aaa19970009b9620c4a1c6113
crisantoiii <crisantotadiosaiii@gmail.com>
2026-07-18 11:52:47 +0800

    configure Product entity persistence

---
+++ src/InventoryPlatform/InventoryPlatform.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using InventoryPlatform.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryPlatform.Infrastructure.Persistence.Configurations;
+
+public sealed class ProductConfiguration
+    : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Sku)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.HasIndex(x => x.Sku)
+            .IsUnique();
+
+        builder.Property(x => x.Barcode)
+            .HasMaxLength(100);
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(1000);
+
+        builder.Property(x => x.Unit)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(x => x.CostPrice)
+            .HasPrecision(18, 2);
+
+        builder.Property(x => x.SellingPrice)
+            .HasPrecision(18, 2);
+
+        builder.Property(x => x.IsActive)
+            .HasDefaultValue(true);
+
+        builder.Property(x => x.CreatedAtUtc)
+            .IsRequired();
+
+        builder.Property(x => x.UpdatedAtUtc);
+    }
+}
--- src/InventoryPlatform/InventoryPlatform.Infrastructure/Persistence/Migrations/20260718033735_AddProduct.Designer.cs
+++ src/InventoryPlatform/InventoryPlatform.Infrastructure/Persistence/Migrations/20260718033735_AddProduct.Designer.cs
@@ -4,16 +4,19 @@ using InventoryPlatform.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace InventoryPlatform.Infrastructure.Migrations
+namespace InventoryPlatform.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260718033735_AddProduct")]
+    partial class AddProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
