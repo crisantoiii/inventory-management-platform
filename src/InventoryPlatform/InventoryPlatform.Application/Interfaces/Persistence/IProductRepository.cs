0101d4632acd80db183fa126777b3d2aa0b2ae04
crisantoiii <crisantotadiosaiii@gmail.com>
2026-07-18 12:03:14 +0800

    add repository contracts

---
+++ src/InventoryPlatform/InventoryPlatform.Application/Interfaces/Persistence/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using InventoryPlatform.Domain.Entities;
+
+namespace InventoryPlatform.Application.Interfaces.Persistence;
+
+public interface IProductRepository : IRepository<Product>
+{
+    Task<Product?> GetBySkuAsync(
+        string sku,
+        CancellationToken cancellationToken = default);
+
+    Task<bool> ExistsBySkuAsync(
+        string sku,
+        CancellationToken cancellationToken = default);
+}
---
+++ src/InventoryPlatform/InventoryPlatform.Application/Interfaces/Persistence/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using InventoryPlatform.Domain.Common;
+using System.Linq.Expressions;
+
+namespace InventoryPlatform.Application.Interfaces.Persistence;
+
+public interface IRepository<TEntity>
+    where TEntity : BaseEntity
+{
+    Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<TEntity>> FindAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken = default);
+
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+    void Update(TEntity entity);
+
+    void Remove(TEntity entity);
+
+    Task<bool> ExistsAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken = default);
+}
