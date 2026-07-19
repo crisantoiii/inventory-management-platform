5d74d19fbc7b4f5def0a94fce01fbc00591cccd4
crisantoiii <crisantotadiosaiii@gmail.com>
2026-07-19 08:02:49 +0800

    implement create product Razor Page

---
+++ src/InventoryPlatform/InventoryPlatform.Web/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using InventoryPlatform.Application.Features.Products.CreateProduct;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InventoryPlatform.Web.Pages.Products;
+
+public class CreateModel : PageModel
+{
+    private readonly CreateProductHandler _handler;
+
+    public CreateModel(CreateProductHandler handler)
+    {
+        _handler = handler;
+    }
+
+    [BindProperty]
+    public CreateProductRequest Product { get; set; } = default!;
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var result = await _handler.HandleAsync(Product);
+
+        if (!result.IsSuccess)
+        {
+            ModelState.AddModelError(
+                string.Empty,
+                result.Error.Message);
+
+            return Page();
+        }
+
+        return RedirectToPage("Index");
+    }
+}
