4cfcaf693d20d4af4f7d2a2a4731f399e4176139
crisantoiii <crisantotadiosaiii@gmail.com>
2026-07-28 09:42:57 +0800

    feat(identity): integrate IdentityDbContext

--- src/InventoryPlatform/InventoryPlatform.Infrastructure/Persistence/Context/ApplicationDBContext.cs
+++ src/InventoryPlatform/InventoryPlatform.Infrastructure/Persistence/Context/ApplicationDBContext.cs
@@ -1,9 +1,13 @@
 ﻿using InventoryPlatform.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using InventoryPlatform.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace InventoryPlatform.Infrastructure.Persistence.Context;
 
-public sealed class ApplicationDbContext : DbContext
+public sealed class ApplicationDbContext
+    : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
 {
     public DbSet<Product> Products => Set<Product>();
     public DbSet<Category> Categories => Set<Category>();
