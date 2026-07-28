c3bf631c689590afd1f4208d8839ccb52679e18b
crisantoiii <crisantotadiosaiii@gmail.com>
2026-07-28 10:08:54 +0800

    feat(identity): integrate ASP.NET Core Identity foundation

--- src/InventoryPlatform/InventoryPlatform.Web/Extensions/ServiceCollectionExtensions.cs
+++ src/InventoryPlatform/InventoryPlatform.Web/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using InventoryPlatform.Infrastructure.Identity;
+using InventoryPlatform.Infrastructure.Persistence.Context;
+using Microsoft.AspNetCore.Identity;
 
 namespace InventoryPlatform.Web.Extensions;
 
@@ -7,6 +10,22 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddWeb(
         this IServiceCollection services)
     {
+        services
+            .AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 8;
+
+                options.User.RequireUniqueEmail = true;
+
+                options.SignIn.RequireConfirmedAccount = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
         services.AddRazorPages();
 
         return services;
