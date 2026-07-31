d0aa10c9780d15d2072d8a3e8c04f8f2de761b5e
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-01 07:31:42 +0800

    feat(users): add update user application layer

---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Users/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,15 @@
+﻿using InventoryPlatform.Application.Interfaces.Identity;
+
+namespace InventoryPlatform.Application.Features.Users.UpdateUser;
+
+public sealed class UpdateUserHandler
+{
+
+    private readonly IIdentityService _identityService;
+
+    public UpdateUserHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+}
---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Users/UpdateUser/UpdateUserRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace InventoryPlatform.Application.Features.Users.UpdateUser;
+
+public sealed record UpdateUserRequest
+{
+    public Guid Id { get; init; }
+
+    public string UserName { get; init; } = string.Empty;
+
+    public string Email { get; init; } = string.Empty;
+
+    public string? PhoneNumber { get; init; }
+
+    public bool EmailConfirmed { get; init; }
+}
