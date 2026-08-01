55f07790c92d2fcad94fe3e291861a85aa6ccf56
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-01 15:59:23 +0800

    feat(users): add reset password application layer

---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Users/ResetPassowrd/ResetPasswordHandler.cs
@@ -0,0 +1,13 @@
+﻿using InventoryPlatform.Application.Interfaces.Identity;
+
+namespace InventoryPlatform.Application.Features.Users.ResetPassowrd;
+
+public sealed class ResetPasswordHandler
+{
+    private readonly IIdentityService _identityService;
+
+    public ResetPasswordHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+}
