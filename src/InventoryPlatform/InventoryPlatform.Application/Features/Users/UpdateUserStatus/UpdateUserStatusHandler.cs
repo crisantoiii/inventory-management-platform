9e7fa238065a843b724c88f584aeec8f85b0e86b
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-01 13:08:57 +0800

    feat(users): add user activation application layer

---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Users/UpdateUserStatus/UpdateUserStatusHandler.cs
@@ -0,0 +1,15 @@
+﻿using InventoryPlatform.Application.Interfaces.Identity;
+
+namespace InventoryPlatform.Application.Features.Users.UpdateUserStatus;
+
+public sealed class UpdateUserStatusHandler
+{
+
+    private readonly IIdentityService _identityService;
+
+    public UpdateUserStatusHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+}
