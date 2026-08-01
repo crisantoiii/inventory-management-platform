9d608f5f7245fee1abcdcf912a75d6ed31430238
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-01 10:10:06 +0800

    feat(users): add update user roles application layer

---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Users/UpdateUserRoles/UpdateUserHandler.cs
@@ -0,0 +1,15 @@
+﻿using InventoryPlatform.Application.Interfaces.Identity;
+
+namespace InventoryPlatform.Application.Features.Users.UpdateUserRoles;
+
+public sealed class UpdateUserRolesHandler
+{
+
+    private readonly IIdentityService _identityService;
+
+    public UpdateUserRolesHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+}
