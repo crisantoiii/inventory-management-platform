fc0083718b4ddadf552316b92910a8efffb9e53e
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-05 07:45:03 +0800

    feat(domain): add purchase order status enum

---
+++ src/InventoryPlatform/InventoryPlatform.Domain/Enums/PurchaseOrderStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryPlatform.Domain.Enums;
+
+public enum PurchaseOrderStatus
+{
+    Draft = 1,
+
+    Submitted = 2,
+
+    Approved = 3,
+
+    Receiving = 4,
+
+    Completed = 5,
+
+    Cancelled = 6
+}
