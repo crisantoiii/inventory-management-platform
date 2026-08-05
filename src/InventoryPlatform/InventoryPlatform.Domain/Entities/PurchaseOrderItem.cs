b0863c0b29b96eb48c606ffa0a8376ad05710e5e
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-05 08:13:01 +0800

    feat(domain): define purchase order item entity

--- src/InventoryPlatform/InventoryPlatform.Domain/Entities/PurchaseOrderItem.cs
+++ src/InventoryPlatform/InventoryPlatform.Domain/Entities/PurchaseOrderItem.cs
@@ -1,5 +1,24 @@
-﻿namespace InventoryPlatform.Domain.Entities;
+﻿using InventoryPlatform.Domain.Common;
 
-public sealed class PurchaseOrderItem
+namespace InventoryPlatform.Domain.Entities;
+
+public sealed class PurchaseOrderItem : BaseEntity
 {
+    private PurchaseOrderItem()
+    {
+    }
+
+    public Guid PurchaseOrderId { get; private set; }
+
+    public PurchaseOrder PurchaseOrder { get; private set; } = null!;
+
+    public Guid ProductId { get; private set; }
+
+    public Product Product { get; private set; } = null!;
+
+    public decimal Quantity { get; private set; }
+
+    public decimal UnitCost { get; private set; }
+
+    public decimal ReceivedQuantity { get; private set; }
 }
