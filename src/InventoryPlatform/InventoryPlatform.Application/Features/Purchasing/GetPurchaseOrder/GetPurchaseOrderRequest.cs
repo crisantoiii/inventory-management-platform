e8b64591aa592ed5419197b3d7341420931c1a84
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-07 13:05:47 +0800

    feat(application): implement get purchase order use case

---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Purchasing/GetPurchaseOrder/GetPurchaseOrderRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace InventoryPlatform.Application.Features.Purchasing.GetPurchaseOrder;
+
+public sealed record GetPurchaseOrderRequest(
+    int Id);
---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Purchasing/GetPurchaseOrder/GetPurchaseOrderResponse.cs
@@ -0,0 +1,14 @@
+﻿using InventoryPlatform.Domain.Enums;
+
+namespace InventoryPlatform.Application.Features.Purchasing.GetPurchaseOrder;
+
+public sealed record GetPurchaseOrderResponse(
+    int Id,
+    int SupplierId,
+    string SupplierName,
+    DateOnly OrderDate,
+    DateOnly? ExpectedDeliveryDate,
+    PurchaseOrderStatus Status,
+    string? Remarks,
+    decimal TotalAmount,
+    IReadOnlyCollection<GetPurchaseOrderItemResponse> Items);
