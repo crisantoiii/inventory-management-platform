d0834a4f3c822f846da31dfc099b3f6e8bd7d34d
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-11 21:29:51 +0800

    feat(account): add self-service password change

---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Account/ChangePassword/ChangePasswordHandler.cs
@@ -0,0 +1,23 @@
+﻿using InventoryPlatform.Application.Interfaces.Identity;
+using InventoryPlatform.Shared.Results;
+
+namespace InventoryPlatform.Application.Features.Account.ChangePassword;
+
+public sealed class ChangePasswordHandler
+{
+    private readonly IAccountService _accountService;
+
+    public ChangePasswordHandler(IAccountService accountService)
+    {
+        _accountService = accountService;
+    }
+
+    public Task<Result> HandleAsync(
+        ChangePasswordRequest request,
+        CancellationToken cancellationToken = default)
+    {
+        return _accountService.ChangePasswordAsync(
+            request,
+            cancellationToken);
+    }
+}
---
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Account/ChangePassword/ChangePasswordRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryPlatform.Application.Features.Account.ChangePassword;
+
+public sealed record ChangePasswordRequest
+{
+    public Guid UserId { get; init; }
+
+    public string CurrentPassword { get; init; } = string.Empty;
+
+    public string NewPassword { get; init; } = string.Empty;
+}
