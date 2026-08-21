e78b97dc0061d1bc30ecdbfb1684e08482d5569b
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-21 08:01:42 +0800

    feat(purchasing): synchronize inventory on receiving

--- src/InventoryPlatform/InventoryPlatform.Application/Features/Purchasing/ReceivingPurchaseOrder/ReceivePurchaseOrderHandler.cs
+++ src/InventoryPlatform/InventoryPlatform.Application/Features/Purchasing/ReceivingPurchaseOrder/ReceivePurchaseOrderHandler.cs
@@ -1,4 +1,6 @@
-﻿using InventoryPlatform.Application.Interfaces.Persistence;
+using InventoryPlatform.Application.Interfaces.Persistence;
+using InventoryPlatform.Domain.Entities;
+using InventoryPlatform.Domain.Enums;
 using InventoryPlatform.Shared.Results;
 
 namespace InventoryPlatform.Application.Features.Purchasing.ReceivePurchaseOrder;
@@ -6,13 +8,19 @@ namespace InventoryPlatform.Application.Features.Purchasing.ReceivePurchaseOrder
 public sealed class ReceivePurchaseOrderHandler
 {
     private readonly IPurchaseOrderRepository _purchaseOrderRepository;
+    private readonly IProductRepository _productRepository;
+    private readonly IInventoryTransactionRepository _inventoryTransactionRepository;
     private readonly IUnitOfWork _unitOfWork;
 
     public ReceivePurchaseOrderHandler(
         IPurchaseOrderRepository purchaseOrderRepository,
+        IProductRepository productRepository,
+        IInventoryTransactionRepository inventoryTransactionRepository,
         IUnitOfWork unitOfWork)
     {
         _purchaseOrderRepository = purchaseOrderRepository;
+        _productRepository = productRepository;
+        _inventoryTransactionRepository = inventoryTransactionRepository;
         _unitOfWork = unitOfWork;
     }
 
@@ -30,9 +38,33 @@ public sealed class ReceivePurchaseOrderHandler
                 PurchaseOrderErrors.NotFound);
         }
 
+        var product = await _productRepository.GetByIdAsync(
+            request.ProductId,
+            cancellationToken);
+
+        if (product is null)
+        {
+            return Result<ReceivePurchaseOrderResponse>.Failure(
+                PurchaseOrderErrors.ProductNotFound(request.ProductId));
+        }
+
         purchaseOrder.Receive(
             request.ProductId,
             request.Quantity);
+
+        product.IncreaseStock(request.Quantity);
+
+        var inventoryTransaction = new InventoryTransaction(
+            request.ProductId,
+            TransactionType.StockIn,
+            request.Quantity,
+            $"PO-{purchaseOrder.Id}",
+            $"Purchase Order {purchaseOrder.Id} receiving",
+            DateTime.UtcNow);
+
+        await _inventoryTransactionRepository.AddAsync(
+            inventoryTransaction,
+            cancellationToken);
 
         await _unitOfWork.SaveChangesAsync(cancellationToken);
 
