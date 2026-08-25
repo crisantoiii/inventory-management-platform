92b0f57d73706d4e1f999492e462d4ae2d477840
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-25 12:42:58 +0800

    refactor(application): reduce verified request redundancy

--- src/InventoryPlatform/InventoryPlatform.Application/Interfaces/Persistence/IInventoryTransactionRepository.cs
+++ src/InventoryPlatform/InventoryPlatform.Application/Interfaces/Persistence/IInventoryTransactionRepository.cs
@@ -1,14 +1,10 @@
-﻿using InventoryPlatform.Application.Interfaces.Persistence;
+using InventoryPlatform.Application.Interfaces.Persistence;
 using InventoryPlatform.Domain.Entities;
 using InventoryPlatform.Shared.Paging;
 
 public interface IInventoryTransactionRepository
     : IRepository<InventoryTransaction>
 {
-    Task AddAsync(
-        InventoryTransaction transaction,
-        CancellationToken cancellationToken = default);
-
     Task<InventoryTransaction?> GetByIdAsync(
         int id,
         CancellationToken cancellationToken = default);
