a0040879d710b26d0bd7aa296e77c937107ba666
crisantoiii <crisantotadiosaiii@gmail.com>
2026-08-30 15:01:27 +0800

    feat(auth): add application authorization abstractions

---
+++ src/InventoryPlatform/InventoryPlatform.Application/Interfaces/Authorization/IAuthorizationGroupRepository.cs
@@ -0,0 +1,16 @@
+using InventoryPlatform.Application.Interfaces.Persistence;
+using InventoryPlatform.Domain.Entities;
+
+namespace InventoryPlatform.Application.Interfaces.Authorization;
+
+public interface IAuthorizationGroupRepository
+    : IRepository<AuthorizationGroup>
+{
+    Task<AuthorizationGroup?> GetWithCapabilitiesAsync(
+        int id,
+        CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<AuthorizationGroup>> GetForUserAsync(
+        Guid userId,
+        CancellationToken cancellationToken = default);
+}
