05768f8820eca24a3ad5e0a5a602a2072cfc9da5
crisantoiii <crisantotadiosaiii@gmail.com>
2026-09-03 12:03:23 +0800

    test: add sprint 11 test infrastructure

---
+++ tests/InventoryPlatform.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace InventoryPlatform.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void UnitTestInfrastructure_IsExecutable()
+    {
+        // Verifies the unit test project compiles and xUnit discovers tests.
+        Assert.True(true);
+    }
+}
