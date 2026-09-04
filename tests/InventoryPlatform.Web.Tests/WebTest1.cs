14d2e89135c1228a2292f750f820442953dacc3a
crisantoiii <crisantotadiosaiii@gmail.com>
2026-09-04 19:38:46 +0800

    test: add Web.Tests project for authorization handler testing

---
+++ tests/InventoryPlatform.Web.Tests/WebTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace InventoryPlatform.Web.Tests;
+
+public class WebTest1
+{
+    [Fact]
+    public void WebTestInfrastructure_IsExecutable()
+    {
+        // Verifies the web test project compiles and xUnit discovers tests.
+        Assert.True(true);
+    }
+}
