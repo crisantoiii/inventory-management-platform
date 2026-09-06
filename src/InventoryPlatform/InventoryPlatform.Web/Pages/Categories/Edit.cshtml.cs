1345a8e440782845b369f66b2ea10e07848eb51e
crisantoiii <crisantotadiosaiii@gmail.com>
2026-09-06 15:58:59 +0800

    fix(authorization): protect Categories Edit page

--- src/InventoryPlatform/InventoryPlatform.Web/Pages/Categories/Edit.cshtml.cs
+++ src/InventoryPlatform/InventoryPlatform.Web/Pages/Categories/Edit.cshtml.cs
@@ -1,10 +1,13 @@
 using InventoryPlatform.Application.Features.Categories.GetCategory;
 using InventoryPlatform.Application.Features.Categories.UpdateCategory;
+using InventoryPlatform.Web.Authorization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace InventoryPlatform.Web.Pages.Categories;
 
+[Authorize(Policy = AuthorizationPolicies.InventoryManagement)]
 public class EditModel : PageModel
 {
     private readonly GetCategoryHandler _getHandler;
